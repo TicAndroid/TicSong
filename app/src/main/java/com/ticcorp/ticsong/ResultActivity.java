642bcc5ab60d3b20cd0c7d03a4d6b851ac76dcbf
jpark <heyheygo77@gmail.com>
2016-08-15 09:50:33 +0900

    나도 진심 뭘 만진지 모르겠다 상태가 이상하다

---
+++ app/src/main/java/com/ticcorp/ticsong/ResultActivity.java
@@ -0,0 +1,17 @@
+package com.ticcorp.ticsong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JPark on 2016-08-15.
+ */
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
